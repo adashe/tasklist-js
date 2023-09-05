bbb062065dac2fa25f12341c6a15674ca470e5f1
Andrea Dashe <andrea.r.dashe@gmail.com>
2023-09-05 11:07:01 -0700

    Added delete card function

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const card = document.querySelector('.card');
 const addCardBtn = document.querySelector('.add-card')
 const cards = document.querySelector('.cards')
 
 addCardBtn.addEventListener('click', () => {
   html = `
-    <div class="card" style="width: 18rem;">
+    <div class="card border-secondary" style="width: 18rem;">
       <div class="card-body">
+        <div class="delete-card">X</div>
         <h5 class="card-title">Card title</h5>
         <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
         <ul class="list-group list-group-flush">
@@ -15,4 +15,10 @@ addCardBtn.addEventListener('click', () => {
   `;
   console.log(html);
   cards.innerHTML += html;
+});
+
+cards.addEventListener('click', e => {
+  if(e.target.classList.contains('delete-card')){
+      e.target.parentElement.parentElement.remove();
+  }
 });
