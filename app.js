701c66fbbcfb96ea100d02f7be4a70b76078c7f0
Andrea Dashe <andrea.r.dashe@gmail.com>
2023-09-05 13:49:15 -0700

    Delete task functionality added

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const addCardBtn = document.querySelector('.add-card');
-const cards = document.querySelector('.cards');
 const addTaskForm = document.querySelector('.add-task');
+const cards = document.querySelector('.cards');
 
 // add card
 addCardBtn.addEventListener('click', () => {
   html = `
-    <div class="card border-secondary" style="width: 18rem;">
-      <div class="card-body">
-        <div class="delete-card">X</div>
-        <h5 class="card-title">Card title</h5>
-        <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-        <ul class="list-group list-group-flush">
-        </ul>
-      </div>
-    </div>
+  <div class="card border-secondary" style="width: 18rem;">
+  <div class="card-body">
+    <div class="delete-card">X</div>
+    <h5 class="card-title">Card title</h5>
+    <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+    <ul class="list-group list-group-flush">
+      </li>
+    </ul>
+    <form class="add-task text-center my-4">
+      <input class="form-control m-auto" type="text" name="add" placeholder="add task" />
+    </form>
+  </div>
+</div>
   `;
   cards.innerHTML += html;
 });
@@ -25,16 +29,31 @@ cards.addEventListener('click', e => {
   }
 });
 
-// add list item
+// add task
 addTaskForm.addEventListener('submit', e => {
   e.preventDefault();
-  message = addTaskForm.add.value.trim();
+  task = addTaskForm.add.value.trim();
   list = e.target.parentElement.querySelector('ul');
   html = `
-  <li class="list-group-item">${message}</li>
+  <li class="list-group-item">
+    <span class="delete-task">X</span>
+    <span>${task}</span>
+  </li>
   `;
   list.innerHTML += html;
   addTaskForm.reset();
 });
 
-// delete list item
+// delete task
+cards.addEventListener('click', e => {
+  if(e.target.classList.contains('delete-task')){
+      e.target.parentElement.remove();
+  }
+});
+
+// add title to new card
+// edit title of existing cards
+// edit existing tasks
+// reorder tasks
+
+// TODO: add task and delete task for new cards
