96745a200b3bb5493fc3432899e1e05271d54f5f
Andrea Dashe <andrea.r.dashe@gmail.com>
2023-09-06 11:45:20 -0700

    Add/delete tasks from added cards

--- app.js
+++ app.js
@@ -1,51 +1,57 @@
 const addCardBtn = document.querySelector('.add-card');
-const addTaskForm = document.querySelector('.add-task');
-const cards = document.querySelector('.cards');
+const addTaskForms = document.querySelectorAll('.add-task');
+const cardsContainer = document.querySelector('.cards-container');
+
 
 // add card
-addCardBtn.addEventListener('click', () => {
-  html = `
-  <div class="card border-secondary" style="width: 18rem;">
-  <div class="card-body">
-    <div class="delete-card">X</div>
-    <h5 class="card-title">Card title</h5>
-    <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-    <ul class="list-group list-group-flush">
-      </li>
-    </ul>
-    <form class="add-task text-center my-4">
-      <input class="form-control m-auto" type="text" name="add" placeholder="add task" />
-    </form>
-  </div>
-</div>
+addCardBtn.addEventListener('click', e => {
+  e.preventDefault();
+  const html = `
+    <div class="card border-secondary" style="width: 18rem;">
+      <div class="card-body">
+        <div class="delete-card">X</div>
+        <h5 class="card-title">TITLE</h5>
+        <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+        <ul class="list-group list-group-flush">
+          </li>
+        </ul>
+        <form class="add-task text-center my-4">
+          <input class="form-control m-auto" type="text" name="add" placeholder="add task" />
+        </form>
+      </div>
+    </div>
   `;
-  cards.innerHTML += html;
+  cardsContainer.innerHTML += html;
 });
 
 // delete card
-cards.addEventListener('click', e => {
+cardsContainer.addEventListener('click', e => {
   if(e.target.classList.contains('delete-card')){
       e.target.parentElement.parentElement.remove();
   }
 });
 
-// add task
-addTaskForm.addEventListener('submit', e => {
+// add task to list using event delegation
+cardsContainer.addEventListener('submit', e => {
+  const form = e.target.closest('.add-task');
   e.preventDefault();
-  task = addTaskForm.add.value.trim();
-  list = e.target.parentElement.querySelector('ul');
-  html = `
-  <li class="list-group-item">
-    <span class="delete-task">X</span>
-    <span>${task}</span>
-  </li>
-  `;
-  list.innerHTML += html;
-  addTaskForm.reset();
-});
 
-// delete task
-cards.addEventListener('click', e => {
+  if(form){
+    task = form.add.value.trim();
+    list = form.parentElement.querySelector('ul');
+    html = `
+      <li class="list-group-item">
+        <span class="delete-task">X</span>
+        <span>${task}</span>
+      </li>
+      `;
+    list.innerHTML += html;
+    form.reset();
+  };
+})
+
+// delete task from list
+cardsContainer.addEventListener('click', e => {
   if(e.target.classList.contains('delete-task')){
       e.target.parentElement.remove();
   }
@@ -55,5 +61,3 @@ cards.addEventListener('click', e => {
 // edit title of existing cards
 // edit existing tasks
 // reorder tasks
-
-// TODO: add task and delete task for new cards
